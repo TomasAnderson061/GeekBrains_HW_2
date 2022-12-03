37985f567fa7f5b3ec92619a6865a12607b360e9
admin <admin@hotmail.com>
2022-12-03 23:30:30 +0500

    Верный код для Решения Задачи №13

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-void SecNum(int num1){
-int tens = num1 / 10 % 10;
-    Console.WriteLine($"Вторая цифра равна - {tens}");
-}
-    Console.WriteLine("Введите число: "); 
-int num1 = Convert.ToInt32(Console.ReadLine());
-SecNum(num1);
+// void SecNum(int num1){
+// int tens = num1 / 10 % 10;
+//     Console.WriteLine($"Вторая цифра равна - {tens}");
+// }
+//     Console.WriteLine("Введите число: "); 
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// SecNum(num1);
 
 // Задача 13: Напишите программу, которая выводит третью цифру
 //  заданного числа или сообщает, что третьей цифры нет.
@@ -17,14 +17,30 @@ SecNum(num1);
 // 78 -> третьей цифры нет
 // 32679 -> 6
 void ThdNum(int num2){
-int hund = num2 / 100 % 10;
-   if (hund==0)
+   int num=num2;
+   int bitdepth_num2 = 0;
+   l1:  if (num>0){
+        num=num/10;
+        bitdepth_num2++;
+      goto l1;
+   }   
+   if (bitdepth_num2<3)
    Console.WriteLine($"Третьей цифры в числе {num2} нет");
    else
-   Console.WriteLine($"Третья цифра в числе {num2} равна - {hund}");
+   {    
+    int thdnum = num2;
+    for (int i=0 ; i<bitdepth_num2-3; i++)
+    {    thdnum = thdnum/10;
+        
+        }
+        thdnum = thdnum%10;
+               
+    Console.WriteLine($"Третья цифра в числе {num2} равна - {thdnum}");
+    }
 }
-   Console.WriteLine("Введите число: "); 
-int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
 ThdNum(num2);
 
 // Задача 15: Напишите программу, которая принимает на вход цифру,
@@ -32,16 +48,16 @@ ThdNum(num2);
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-void NumbDay(int a){
-        if (1<=a&&a<6)
-        Console.WriteLine("Нет");
-        else 
-        if (a==6||a==7)   
-        Console.WriteLine("Да");
-        else
-        Console.WriteLine("Неверный день недели"+" "+";-P");
-}
-Console.WriteLine("Введите число дня недели: "); 
-int num3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Выходной?: ");
-NumbDay(num3);
+// void NumbDay(int a){
+//         if (1<=a&&a<6)
+//         Console.WriteLine("Нет");
+//         else 
+//         if (a==6||a==7)   
+//         Console.WriteLine("Да");
+//         else
+//         Console.WriteLine("Неверный день недели"+" "+";-P");
+// }
+// Console.WriteLine("Введите число дня недели: "); 
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Выходной?: ");
+// NumbDay(num3);
